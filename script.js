90244a9fe7d441e66bb3efbddcc584fd397412bb
Arif <kanebrian0634@gmail.com>
2022-05-11 00:54:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+exchangeIcon = document.querySelector(".exchange"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i")
+
+selectTag.forEach((tag, id) => {
+    for (const country_code in countries) {
+        let selected;
+        if(id==0 && country_code == "en-GB") {
+            seelected = "selected"
+        } else if(id == 1 && country_code == "tr-TR") {
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); //adding options tag inside select tag
+    }
+});
+
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    selectTag[0].value = selectTag[1].value;
+    toText.value = tempText;
+    selectTag[1].value =  tempLang;
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+    });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
+                
+            }
+        }
+
+    });
+});
